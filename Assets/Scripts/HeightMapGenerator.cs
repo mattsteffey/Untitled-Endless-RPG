0edf83bb2ef92819d06b5c3862b0e8b56e0bc9bf
mattsteffey <matthew.steffey@gmail.com>
2024-03-26 03:05:44 -0400

    some progress on biome blending.

--- Assets/Scripts/HeightMapGenerator.cs
+++ Assets/Scripts/HeightMapGenerator.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
-using UnityEngine.Rendering;
+
 
 public static class HeightMapGenerator {
 
@@ -18,9 +15,11 @@ public static class HeightMapGenerator {
         float[,] tempValues = Noise.GenerateNoiseMap(width, height, tempSettings.noiseSettings, sampleCentre);
         float[,] humidityValues = Noise.GenerateNoiseMap(width, height, humiditySettings.noiseSettings, sampleCentre);
 
+        List<TerrainToBlend> terrainToBlendList = new List<TerrainToBlend>();
 
-        float[,] adjustedHeightValues = new float[width, height];
+
         // Copy values from heightValues to adjustedHeightValues
+        float[,] adjustedHeightValues = new float[width, height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 adjustedHeightValues[i, j] = heightValues[i, j];
@@ -32,26 +31,21 @@ public static class HeightMapGenerator {
         for (int i = 0; i < biomeDataList.Count; i++) {
             float[,] biomeNoise = Noise.GenerateNoiseMap(width, height, biomeDataList[i].biomeHeightMaps[0].noiseSettings, sampleCentre);
             biomeNoiseValues.Add(biomeNoise);
-        }
+            }
 
+
+        // Find Biomes
+        int oldBiomeValue = 0;
+        int currentBiomeValue = 0;
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                
-                float biomeHeightValue = new float();
-                float biomeHeightMultiplier = new float();
-
-                float oldBiomeValue = -1;
-                float currentBiomeValue = -2;
-
 
                 for (int k = 0; k < biomeDataList.Count; k++) {
                     if (k < biomeDataList.Count) {
                         if (heightValues[i, j] * heightCurve_threadsafe.Evaluate(heightValues[i, j]) >= biomeDataList[k].startHeight) {
                             if (tempValues[i, j] >= biomeDataList[k].startTemp) {
                                 if (humidityValues[i, j] >= biomeDataList[k].startHumidity) {
-                                    biomeHeightValue = biomeNoiseValues[k][i, j];
-                                    biomeHeightMultiplier = biomeDataList[k].biomeHeightMaps[0].heightMultiplier;
                                     currentBiomeValue = k;
                                     }
                                 }
@@ -59,17 +53,23 @@ public static class HeightMapGenerator {
                         }
                     }
 
-                if (oldBiomeValue != currentBiomeValue) {
-                    if (i > 0 && i < width && j > 0 && j < height) {
-                        adjustedHeightValues[i, j] = (heightValues[i, j] + heightValues[i - 1, j - 1])/2;
-                        }
-                    }
-
-                oldBiomeValue = currentBiomeValue;
-
-                // issue is the height values are processed after this which is fucking with the calculations when comparing them to the previous values...
-                adjustedHeightValues[i, j] *= heightCurve_threadsafe.Evaluate(adjustedHeightValues[i, j]) * heightSettings.heightMultiplier + (biomeHeightValue * biomeHeightMultiplier);
    
+                        TerrainToBlend terrainPoint = new TerrainToBlend();
+                        terrainPoint.blendPointsX = i;
+                        terrainPoint.blendPointsY = j;
+                        terrainPoint.currentTerrain = currentBiomeValue;
+                        terrainPoint.previousTerrain = oldBiomeValue;
+                        terrainToBlendList.Add(terrainPoint);                     
+                        oldBiomeValue = currentBiomeValue;
+                        
+
+
+                if (heightValues[i, j] > maxValue) {
+                    maxValue = heightValues[i, j];
+                    }
+                if (heightValues[i, j] < minValue) {
+                    minValue = heightValues[i, j];
+                    }
 
                 if (adjustedHeightValues[i, j] > maxValue) {
                     maxValue = adjustedHeightValues[i, j];
@@ -81,8 +81,44 @@ public static class HeightMapGenerator {
             }
 
 
+        for (int t = 0; t < terrainToBlendList.Count; t++) {
+           int currentTerrain = terrainToBlendList[t].currentTerrain;
+           int previousTerrain = terrainToBlendList[t].previousTerrain;
+           int blendPointsX = terrainToBlendList[t].blendPointsX;
+           int blendPointsY = terrainToBlendList[t].blendPointsY;
 
-                return new HeightMap(adjustedHeightValues, tempValues, humidityValues, minValue, maxValue);
+
+
+
+
+            if (currentTerrain != previousTerrain) {
+
+                for (int i = -2; i <= 2; i++) {
+                    for (int j = -2; j <= 2; j++) {
+                        int newX = blendPointsX + i;
+                        int newY = blendPointsY + j;
+                        //Keep these 2's here, they are the border - until you are ready to iterate over to other chunks...
+                        if (newX >= 2 && newX < height - 2 && newY >= 2 && newY < width - 2) {
+                            adjustedHeightValues[newX, newY] = heightCurve_threadsafe.Evaluate(heightValues[newX, newY]) * heightSettings.heightMultiplier + ((biomeNoiseValues[currentTerrain][newX, newY] 
+                                * biomeDataList[currentTerrain].biomeHeightMaps[0].heightMultiplier + biomeNoiseValues[previousTerrain][newX, newY] * biomeDataList[previousTerrain].biomeHeightMaps[0].heightMultiplier) / 2);
+                            }
+                        else {
+                            adjustedHeightValues[blendPointsX, blendPointsY] = heightCurve_threadsafe.Evaluate(heightValues[blendPointsX, blendPointsY]) * heightSettings.heightMultiplier + (biomeNoiseValues[currentTerrain][blendPointsX, blendPointsY]
+                              * biomeDataList[currentTerrain].biomeHeightMaps[0].heightMultiplier);
+                            }
+                        }
+                    }
+                }
+
+            if (currentTerrain == previousTerrain) {
+                adjustedHeightValues[blendPointsX, blendPointsY] = heightCurve_threadsafe.Evaluate(heightValues[blendPointsX, blendPointsY]) * heightSettings.heightMultiplier + (biomeNoiseValues[currentTerrain][blendPointsX, blendPointsY]
+                               * biomeDataList[currentTerrain].biomeHeightMaps[0].heightMultiplier);
+                }
+         }
+
+
+
+        return new HeightMap(adjustedHeightValues, tempValues, humidityValues, minValue, maxValue);
         }
     }
 
@@ -100,5 +136,21 @@ public struct HeightMap {
         this.humidityValues = humidityValues;
         this.minValue = minValue;
         this.maxValue = maxValue;
+        }
+    }
+
+public struct TerrainToBlend {
+    public int blendPointsX;
+    public int blendPointsY;
+    public int currentTerrain;
+    public int previousTerrain;
+    public float blendedTerrainValue;
+
+    public TerrainToBlend(int blendPointsX, int blendPointsY, int currentTerrain, int previousTerrain, float blendedTerrainValue) {
+        this.blendPointsX = blendPointsX;
+        this.blendPointsY = blendPointsY;
+        this.currentTerrain = currentTerrain;
+        this.previousTerrain = previousTerrain;
+        this.blendedTerrainValue = blendedTerrainValue;
         }
     }
